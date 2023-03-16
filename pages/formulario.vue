7eec9986f5184c3358c1eba36dbd3f555a2a6b44
Tamires01-alt <rtamires117@gmail.com>
2023-03-16 01:44:19 -0300

    subindo codigo

---
+++ pages/formulario.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div>
+      <b-table striped hover :items="items"></b-table>
+    </div>
+    <div>
+      <b-button variant="info">Show</b-button>
+      <b-button variant="primary">Edit</b-button>
+      <b-button variant="danger">Delete</b-button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        items: [
+          { id: 1, name: 'no pets', active: 1, order: 2 },
+          { id: 2, name: 'no pets', active: 1, order: 2 },
+          { id: 3, name: 'no pets', active: 1, order: 2 },
+          { id: 4, name: 'no pets', active: 1, order: 2 }
+        ]
+      }
+    }
+  }
+</script>
---
+++ pages/login.vue
@@ -0,0 +1,41 @@
+<template>
+  <b-container>
+    <b-row class="justify-content-md-center">
+      <b-col md="6">
+        <b-card title="Login">
+          <b-form @submit.prevent="login">
+            <b-form-group
+              label="E-mail"
+              label-for="email-input"
+              description="Digite o seu endereço de e-mail"
+            >
+              <b-form-input
+                id="email-input"
+                v-model="email"
+                type="email"
+                required
+              ></b-form-input>
+            </b-form-group>
+
+            <b-form-group
+              label="Senha"
+              label-for=""
+              description="Digite a sua senha"
+            >
+              <b-form-input
+                id=""
+                type="password"
+                required
+              ></b-form-input>
+            </b-form-group>
+
+            <b-button type="submit" variant="primary">Entrar</b-button>
+          </b-form>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+</script>
